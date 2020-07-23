b976652a821c7419591181604844f31231210823
phelippemac <phelippemac@hotmail.com>
2020-07-22 23:48:35 -0300

    Birthday Chocolate Added

---
+++ BirthdayChocolate.rb
@@ -0,0 +1,18 @@
+#Challange link = https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+require 'json'
+require 'stringio'
+
+# Complete the birthday function below.
+def birthday(s, d, m)
+  slices = []
+  s.each_index do |i|
+    val = 0
+    m.times do |part|
+      next if s[i+part].nil?
+      val += s[i+part]
+    end
+    slices << val
+  end
+  p slices.map! {|x| x == d ? x : next}.compact.size
+end
