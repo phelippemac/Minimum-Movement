f56585d9613386fd31a031c32c3ac0a7080231e7
phelippemac <phelippemac@hotmail.com>
2020-08-01 23:38:46 -0300

    Kangoroo added

---
+++ Kangoroo.rb
@@ -0,0 +1,21 @@
+# Challange Link = https://www.hackerrank.com/challenges/kangaroo/problem
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2)
+  return 'NO' if (x2 > x1 && v2 > v1) || (x1 > x2 && v1 > v2)
+
+  jump1 = Array.new(0)
+  jump2 = Array.new(0)
+  (x2 - x1).times do |time|
+    jump1 << [x1 + v1, time]
+    x1 += v1
+    jump2 << [x2 + v2, time]
+    x2 += v2
+  end
+  p jump1 & jump2
+  p(jump1 & jump2).empty? ? 'NO' : 'YES'
+end
