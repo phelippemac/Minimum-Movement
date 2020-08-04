59e38f7c5ef99523bc6cf6ce7160b5105df8a999
phelippemac <phelippemac@hotmail.com>
2020-08-03 22:00:38 -0300

    MigratoryBirdes added'

---
+++ MigratoryBirds.rb
@@ -0,0 +1,18 @@
+# Challange Link = https://www.hackerrank.com/challenges/migratory-birds/problem
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the migratoryBirds function below.
+def migratoryBirds(arr)
+  birds = Hash.new(0)
+  arr.each do |bir|
+    birds.key?(bir) ? birds[bir] += 1 : birds[bir] = 1
+  end
+  max = birds.max_by { |_key, value| value }.last
+  birds.sort_by { |key, _value| key }
+         .select { |_key, value| value == max }
+         .first
+         .first
+end
